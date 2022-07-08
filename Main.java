8e3f246b923eceae57bda07ba5a1cea199e5590c
noelly1232 <59965675+NoellyCosta@users.noreply.github.com>
2022-07-08 19:33:30 +0000

    Add switch with year seasons

--- Main.java
+++ Main.java
@@ -1,28 +1,62 @@
 class Main{
-  
-  //Car
-  public static void main(String[] args) {
-    Car myCar = new Car();
-    myCar.AddDoors();
-     myCar.AddDoors();
-     myCar.AddDoors();
-     myCar.AddDoors();
-    System.out.println(myCar.doors);
-    }
 
-  public static int sum(int a, int b, int c){
-   return a + b + c;
+
+  //If
+ public static void main(String[] args){
+
+  int numberIf = 2;
+
+   if (numberIf < 0){
+     System.out.println("A variable" + numberIf +  "is a negative number");
+  }else if (numberIf > 0){
+     System.out.println("A variable" +  numberIf  + "is a positive number");
+  } else {
+     System.out.println("NAN");
   }
+
+
+   //while
+   int numberWhile = 0;
+
+   while (numberWhile < 3){
+     numberWhile ++;
+     System.out.println("The variable is equivalent to " + numberWhile);
+   }
+
+//Do While
+   int numDoWhile = 1;
+
+   do{
+     numDoWhile++;
+     System.out.println("The numDoWhile is now valid as " + numDoWhile);
+   }while(numDoWhile < 3);
  
-}
+ 
+  //For
+  for(int numberFor = 0; numberFor <= 3; numberFor++){
+  System.out.println("The numberFor is now valid as " + numberFor);
+  }
 
-class Car {
-  public int doors = 0;
-  
-public void AddDoors(){
-  this.doors++;
-}
+   //Switch 
+
+   var season = "Spring";
+
+   switch(season){
+     case "Spring":
+       System.out.println("The season is a Spring");
+     break;
+     case "Summer":
+       System.out.println("The season is a Summer");
+     break;
+     case "Autumn" :
+       System.out.println("The season is a Autumn");
+     break;
+     case "Winter" :
+       System.out.println("The season is winter");
+     default:
+       System.out.println(season);
+   }
+ }
 
 
-  
 }
