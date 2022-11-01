39c0120422e898df90e0497d0317b674a67fab7b
noelly1232 <59965675+NoellyCosta@users.noreply.github.com>
2022-11-01 16:23:29 +0000

    add credit a people customer and salary as people employee.

--- Main.java
+++ Main.java
@@ -1,29 +1,35 @@
-class Main{
+public class Main {
 
-public static void main (String[] args){
-  People age = new People();
-  People name = new People();
-  People cellPhone = new People();
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Employee employee = new Employee();
+        customer.name = "Orlando";
+        customer.age = 43;
+        customer.phoneNumber = 55555555;
+        customer.credit = 225.5;
+        System.out.println("I'm " + customer.name + ", I have " + customer.age + "  years old and my phone number is " + customer.phoneNumber
+                + " and my available credit is " + customer.credit + "$");
 
-  age.setData("20");
-  name.setData("Lea");
-  cellPhone.setData("555-5555");
-  
-   System.out.println(age.getData());
-   System.out.println(name.getData());
-  System.out.println(cellPhone.getData());
-   
- 
+        employee.name = "Lina";
+        employee.age = 32;
+        employee.phoneNumber = 34567634;
+        employee.salary = 4000.50;
+        System.out.println("I'm " + employee.name + ", and I have " + employee.age + "years old and my phone number is " + employee.phoneNumber
+                + " and my salary is " + employee.salary + "$");
+
+    }
 }
-}
-class People {
-  private String data;
-  
-  public void setData(String data){
-    this.data = data;
-  }
 
-  public String getData(){
-    return this.data;
-  }
+class People{
+    int age;
+    int phoneNumber;
+    String name;
+}
+
+class Customer extends People{
+    double credit;
+}
+
+class Employee extends People {
+    double salary;
 }
